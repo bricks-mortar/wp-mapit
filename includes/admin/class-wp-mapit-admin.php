95d54c2cf8803026ee66d6869dde84cb139653b1
Dane Grant <danecando@gmail.com>
2015-06-26 20:58:18 -0400

    support page

--- includes/admin/class-wp-mapit-admin.php
+++ includes/admin/class-wp-mapit-admin.php
@@ -41,14 +41,17 @@ class WP_MapIt_Admin {
 	 * @return void
 	 */
 	public function admin_menu() {
-		// Top level menu item
+		// Parent Menu -> Map Listings4
 		add_menu_page( __( 'Maps', 'wp-mapit' ), __( 'MapIt', 'wp-mapit' ), 'manage_options', 'mapit-maps', array( $this, 'maps_template' ), 'dashicons-location-alt', 99 );
 
-		// Map listing page
+		// Map listings
 		add_submenu_page ('mapit-maps', __( 'Maps', 'wp-mapit' ), __( 'Maps', 'wp-mapit' ), 'manage_options', 'mapit-maps', array( $this, 'maps_template' ) );
 
 		// Settings
 		add_submenu_page( 'mapit-maps', __( 'WP MapIt Settings', 'wp-mapit' ), __( 'Settings', 'wp-mapit' ), 'manage_options', 'mapit-settings', array( $this->settings_page, 'output' ) );
+
+		// Support
+		add_submenu_page( 'mapit-maps', __( 'WP MapIt Support', 'wp-mapit' ), __( 'Support', 'wp-mapit' ), 'manage_options', 'mapit-support', array( $this, 'support_template' ) );
 
 	}
 
@@ -70,12 +73,32 @@ class WP_MapIt_Admin {
 		echo '<form id="maps-filter" method="get">';
 		echo    '<table class="wp-list-table widefat fixed striped posts">';
 		echo 	    '<thead><tr>';
+		echo        '<td>ID</td><td>Name</td><td>Shortcode</td><td>Actions</td>';
 		echo        '</tr></thead>';
 		echo        '<tbody id="the-list"><p>You havent created any maps yet!</p></tbody>';
 		echo 	    '<tfoot><tr>';
 		echo        '</tr></tfoot>';
 		echo    '</table>';
 		echo '</form>';
+		echo '</div>';
+	}
+
+	public function support_template() {
+		echo '<div class="wrap">';
+		echo '<h2>WP MapIt Support</h2>';
+		echo '<p>Thanks for download WP MapIt! This plugin is actively developed
+				and maintained by <a href="http://bricksandmortarweb.com">Bricks
+				& Mortar Creative</a>.';
+		echo '<div class="admin-page-content">';
+		echo '<br><h3>Follow the link that corresponds to your issue:</h3>';
+		echo '<ul>';
+		echo '<li><strong>Documentation:</strong> <a href="https://github.com/bricks-mortar/wp-mapit">https://github.com/bricks-mortar/wp-mapit</a></li>';
+		echo '<li><strong>Report Issue:</strong> <a href="https://github.com/bricks-mortar/wp-mapit/issues">https://github.com/bricks-mortar/wp-mapit/issues</a></li>';
+		echo '</ul><ul>';
+		echo '<li><strong>Contribute:</strong> This project is open source on GitHub.
+				Please feel free to open any issues or PRs.';
+		echo '<li><strong>Custom Development:</strong> <a href="http://bricksandmortarweb.com">Developer Website</a> - <a href="mailto:info@bricksandmortarweb.com">Email Contact</a></li>';
+		echo '</ul></div>';
 		echo '</div>';
 	}
 }
